cfc80be9c652e7bff15c8a02c228fa2f05fbdfcc
sunny911-max <realsun911@gmail.com>
2025-07-14 01:05:47 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,8 +1,8 @@
 import os
 import requests
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+BOT_TOKEN = os.getenv("7307067620:AAEOHrNskxLEWOcMKvuKtVbrJUYpD0zokMA")
+CHAT_ID = os.getenv("HighRisk_scalper_bot")
 
 def send_telegram_message(message):
     if not BOT_TOKEN or not CHAT_ID:
