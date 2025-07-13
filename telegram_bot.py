c52c0f0e3ef04bc90f2efff5b4fb4f90e5ebe79d
sunny911-max <realsun911@gmail.com>
2025-07-14 04:05:17 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,5 @@
 import requests
+import traceback
 
 BOT_TOKEN = "7750164805:AAE1H7HB5aeq5-a8onpZiIZ1PJM7SNGI1Po"
 CHAT_ID = "7545235284"
@@ -17,3 +18,4 @@ def send_telegram_message(message):
         print("📬 Telegram response body:", response.text)
     except Exception as e:
         print("❌ Error sending message:", str(e))
+        traceback.print_exc()
