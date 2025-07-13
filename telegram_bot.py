1154898cab348a79e14340ba90909dfc3c966aa8
sunny911-max <realsun911@gmail.com>
2025-07-14 04:02:28 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,6 +4,7 @@ BOT_TOKEN = "7750164805:AAE1H7HB5aeq5-a8onpZiIZ1PJM7SNGI1Po"
 CHAT_ID = "7545235284"
 
 def send_telegram_message(message):
+    print("📨 Preparing to send message...")
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {
         "chat_id": CHAT_ID,
