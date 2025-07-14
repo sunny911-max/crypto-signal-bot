16defb0a4e582313a7aea5eaa7784eac3581effe
sunny911-max <realsun911@gmail.com>
2025-07-14 14:27:35 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,9 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "✅ Ssniper bot is running with new token."
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    return "Crypto bot is running!"
